8fd969fa51da3d59cdc41f26830e171938f87406
roman.ulianov <dev.roman.ulianov@gmail.com>
2019-08-17 16:56:01 +0300

    Swagger added

--- CustomerInquiry/Controllers/CustomerController.cs
+++ CustomerInquiry/Controllers/CustomerController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace CustomerInquiry.Controllers
 {
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class CustomerController : Controller
     {
         // GET api/values
         [HttpGet]
--- CustomerInquiry/Startup.cs
+++ CustomerInquiry/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace CustomerInquiry
 {
@@ -24,6 +25,12 @@ namespace CustomerInquiry
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Customer Inquiry", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,6 +40,17 @@ namespace CustomerInquiry
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Customer Inquiry V1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseMvc();
         }
